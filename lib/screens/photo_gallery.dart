7ec1122bfb8b7143283ff9002639fbefc7deb091
Serg8787 <tsybulnikserg@gmail.com>
2022-10-22 12:48:40 +0200

    add progress bar

--- lib/screens/photo_gallery.dart
+++ lib/screens/photo_gallery.dart
@@ -3,20 +3,17 @@ import 'package:photo_gallery/keys.dart';
 import 'package:photo_gallery/services/network_helper.dart';
 
 class PhotoGallery extends StatefulWidget {
-
-
   @override
   State<PhotoGallery> createState() => _PhotoGalleryState();
 }
 
 class _PhotoGalleryState extends State<PhotoGallery> {
-
-  // Future<List<String>>? images;
-  List<String>? images;
+  Future<List<String>>? images;
 
   Future<List<String>> getImages() async {
     List<String> images = [];
-    String url = "https://pixabay.com/api/?key=$pixabayApiKey&per_page=30&image_type=photo";
+    String url =
+        "https://pixabay.com/api/?key=$pixabayApiKey&per_page=30&image_type=photo";
     NetworkHelper networkHelper = NetworkHelper(url: url);
     dynamic data = await networkHelper.getData();
     List<dynamic> hitsList = data["hits"] as List;
@@ -27,24 +24,41 @@ class _PhotoGalleryState extends State<PhotoGallery> {
     return images;
   }
 
-    @override
-    void initState() {
-      super.initState();
-     // images = getImages();
-      getImages().then((value) => print(value));
-    }
+  @override
+  void initState() {
+    super.initState();
+    images = getImages();
+    // getImages().then((value) => print(value));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                crossAxisSpacing: 6,
-                mainAxisSpacing: 6), itemBuilder: (context, index) {
-          return Container();
-        }),
+      body: FutureBuilder(
+        future: images,
+        builder: (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.active:
+            case ConnectionState.waiting:
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+              break;
+            case ConnectionState.done:
+              return SafeArea(
+                child: GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3,
+                        crossAxisSpacing: 6,
+                        mainAxisSpacing: 6),
+                    itemBuilder: (context, index) {
+                      return Container();
+                    }),
+              );
+              break;
+          }
+        },
       ),
     );
   }
