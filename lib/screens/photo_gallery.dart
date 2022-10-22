7f4c4d2476f9b0ef1b88d9172dcfd8e1c225d44b
Serg8787 <tsybulnikserg@gmail.com>
2022-10-22 10:58:23 +0200

    get data = list images

--- lib/screens/photo_gallery.dart
+++ lib/screens/photo_gallery.dart
@@ -11,15 +11,26 @@ class PhotoGallery extends StatefulWidget {
 
 class _PhotoGalleryState extends State<PhotoGallery> {
 
-  List<String>? images;
+  Future<List<String>>? images;
 
   Future<List<String>> getImages() async {
-
     List<String> images = [];
     String url = "https://pixabay.com/api/?key=$pixabayApiKey&per_page=30&image_type=photo";
     NetworkHelper networkHelper = NetworkHelper(url: url);
     dynamic data = await networkHelper.getData();
+    List<dynamic> hitsList = data["hits"] as List;
 
+    for (int i = 0; i < hitsList.length; i++) {
+      images.add(hitsList[i]["largeImageUrl"]);
+    }
+    return images;
+  }
+
+    @override
+    void initState() {
+      super.initState();
+     images = getImages();
+    }
   }
   @override
   Widget build(BuildContext context) {
