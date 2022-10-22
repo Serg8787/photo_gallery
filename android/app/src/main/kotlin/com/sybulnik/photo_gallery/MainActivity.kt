87529cd43a53d9819fadb03b4515755bd89af6f6
Serg8787 <tsybulnikserg@gmail.com>
2022-10-22 09:58:04 +0200

    initial& add first code

---
+++ android/app/src/main/kotlin/com/sybulnik/photo_gallery/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sybulnik.photo_gallery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/keys.dart
@@ -0,0 +1,3 @@
+
+
+const String pixabayApiKey = "30778076-5d4325e7474bf973987be42f2";
